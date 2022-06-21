89b84e951f6424b4c0c26ff973cd8b6dbbda2696
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-06-21 20:24:21 -0300

    Reimplementação das configurações e na classe Producer do Kafka

--- src/main/java/br/com/school/financialservice/producer/ClientProducer.java
+++ src/main/java/br/com/school/financialservice/producer/ClientProducer.java
@@ -1,9 +1,11 @@
 package br.com.school.financialservice.producer;
 
 import br.com.school.financialservice.domain.entity.Client;
+import com.netflix.discovery.converters.Auto;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClientProducer {
     private static final Logger logger = LoggerFactory.getLogger(ClientProducer.class);
-    private final String topic;
-    private final KafkaTemplate<String, Client> kafkaTemplate;
+    private static final String topic = "clients";
 
-    public void send(Client client){
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String client){
         kafkaTemplate.send(topic, client).addCallback(
                 success -> logger.info("Message send."),
                 failure -> logger.info("Message failed")
